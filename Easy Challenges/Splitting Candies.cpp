3a633817d1113a3c15c07897366d750a448eb0ac
Akash_Verma <akashverma2404@gmail.com>
2020-02-04 18:54:33 +0530

    Create Splitting Candies.cpp

---
+++ Easy Challenges/Splitting Candies.cpp
@@ -0,0 +1,29 @@
+/*Cyael is a teacher at a very famous school in Byteland and she is known by her students for being very polite to them and also to encourage them to get good marks on their tests.
+
+Then, if they get good marks she will reward them with candies :) However, she knows they are all very good at Mathematics, so she decided to split the candies evenly to all the students she considers worth of receiving them, so they don't fight with each other.
+
+She has a bag which initially contains N candies and she intends to split the candies evenly to K students. To do this she will proceed as follows: while she has more than K candies she will give exactly 1 candy to each student until she has less than K candies. On this situation, as she can't split candies equally among all students she will keep the remaining candies to herself.
+
+Your job is to tell how many candies will each student and the teacher receive after the splitting is performed. */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    long long int n,k,rem;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> k;
+        if(k == 0)
+        {
+            cout << k << " " << n << endl;
+        }
+        else
+        {
+            rem = n % k;
+            n = n - rem;
+            cout << n/k << " " << rem << endl;
+        }
+    }
+}
