fa98fbc5ae5c529d44ae1a9c202ccbda63ee28dc
Akash_Verma <akashverma2404@gmail.com>
2020-04-08 21:47:31 +0530

    Create Number Mirror.cpp

---
+++ Beginner Challenges/Number Mirror.cpp
@@ -0,0 +1,14 @@
+/*
+Write a program that accepts a number, n, and outputs the same.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<n;
+    // your code goes here
+    return 0;
+}
